0ef275e7a58f4240b4cfbee185b87611a78fedf6
Munir Wanis <fallen.angel.pp@gmail.com>
2017-03-30 12:36:12 -0300

    :construction: Changes csproj to copy xml after build and test deserialization

--- ADS1T1M.TP3.MunirXavierWanis.Presentation.ConsoleApp/Program.cs
+++ ADS1T1M.TP3.MunirXavierWanis.Presentation.ConsoleApp/Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
+using ADS1T1M.TP3.MunirXavierWanis.Domain.Entities;
 
 namespace ADS1T1M.TP3.MunirXavierWanis.Presentation.ConsoleApp
 {
@@ -10,7 +13,17 @@ namespace ADS1T1M.TP3.MunirXavierWanis.Presentation.ConsoleApp
     {
         static void Main(string[] args)
         {
-            var baseDirectory = $@"{AppDomain.CurrentDomain.BaseDirectory}\Data\exporte-alunos.xml";
+            var serializer = new XmlSerializer(typeof(AlunosList));
+            var alunos = new AlunosList();
+            using (var fs = new FileStream(Directories.ExportAlunoDirectory, FileMode.Open))
+            {
+                alunos = (AlunosList) serializer.Deserialize(fs);
+            }
+            foreach (var aluno in alunos.Alunos)
+            {
+                Console.WriteLine($"{aluno.Birthdate}, {aluno.Cpf}, {aluno.Enabled}, {aluno.Enrollment}, {aluno.Identifier}, {aluno.Name}");
+            }
+            Console.ReadKey();
         }
     }
 }
