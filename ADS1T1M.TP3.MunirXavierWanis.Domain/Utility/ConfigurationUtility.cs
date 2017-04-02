bc4e4b82a7c34a117d74d67e64512a3fde611c7f
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-02 00:31:13 -0300

    :construction: Adds ConfigurationUtility

---
+++ ADS1T1M.TP3.MunirXavierWanis.Domain/Utility/ConfigurationUtility.cs
@@ -0,0 +1,16 @@
+﻿using ADS1T1M.TP3.MunirXavierWanis.Domain.Entities;
+using System;
+using System.Configuration;
+
+namespace ADS1T1M.TP3.MunirXavierWanis.Domain.Utility
+{
+    public class ConfigurationUtility : IConfigurationUtility
+    {
+        public ConfigurationUtility()
+        {
+            AppDomain.CurrentDomain.SetData("DataDirectory", Directories.ProjectDataDirectory);
+        }
+
+        public string ConnectionString { get; } = ConfigurationManager.ConnectionStrings["AlunosConnection"].ConnectionString;
+    }
+}
---
+++ ADS1T1M.TP3.MunirXavierWanis.Domain/Utility/IConfigurationUtility.cs
@@ -0,0 +1,7 @@
+﻿namespace ADS1T1M.TP3.MunirXavierWanis.Domain.Utility
+{
+    public interface IConfigurationUtility
+    {
+        string ConnectionString { get; }
+    }
+}
