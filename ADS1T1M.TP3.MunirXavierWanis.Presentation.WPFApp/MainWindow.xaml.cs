9c773021bf88aca7d9f3aeb35fe7eeab51a2fd98
Munir Wanis <fallen.angel.pp@gmail.com>
2017-04-04 10:53:25 -0300

    :bug: Fixes clear text on wpf

--- ADS1T1M.TP3.MunirXavierWanis.Presentation.WPFApp/MainWindow.xaml.cs
+++ ADS1T1M.TP3.MunirXavierWanis.Presentation.WPFApp/MainWindow.xaml.cs
@@ -25,14 +25,10 @@ namespace ADS1T1M.TP3.MunirXavierWanis.Presentation.WPFApp
 
                 if (aluno == null)
                 {
-                    TxtBl_InfoMessage.Text = "Aluno não cadastrado";
+                    this.Clear();
+                    TxtBl_InfoMessage.Text = "Aluno não Encontrado";
                     TxtBl_InfoMessage.Background = Brushes.Blue;
                     TxtBl_InfoMessage.Visibility = Visibility.Visible;
-
-                    TxtBl_BirthdateValue.Text = string.Empty;
-                    TxtBl_CpfValue.Text = string.Empty;
-                    TxtBl_EnrollmentValue.Text = string.Empty;
-                    TxtBl_NameValue.Text = string.Empty;
                 }
                 else
                 {
@@ -61,6 +57,20 @@ namespace ADS1T1M.TP3.MunirXavierWanis.Presentation.WPFApp
                 TxtBl_InfoMessage.Background = Brushes.Red;
                 TxtBl_InfoMessage.Visibility = Visibility.Visible;
             }
+        }
+
+        private void Txt_Enrollment_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+            this.Clear();
+        }
+
+        private void Clear()
+        {
+            TxtBl_BirthdateValue.Text = string.Empty;
+            TxtBl_CpfValue.Text = string.Empty;
+            TxtBl_EnrollmentValue.Text = string.Empty;
+            TxtBl_NameValue.Text = string.Empty;
+            TxtBl_InfoMessage.Visibility = Visibility.Hidden;
         }
     }
 }
